f7937a34e44acbca7b25b94b268d38a50ac57ad7
seoyoungshin <seoyoung@enhans.ai>
2025-07-19 18:01:57 +0900

    - init

---
+++ src/main/kotlin/com/todaystock/api/TodaystockApplication.kt
@@ -0,0 +1,11 @@
+package com.todaystock.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodaystockApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodaystockApplication>(*args)
+}
---
+++ src/test/kotlin/com/todaystock/api/todaystock/TodaystockApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.todaystock.api.todaystock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodaystockApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
