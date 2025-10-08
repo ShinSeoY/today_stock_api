591b39f11060e4e2d3c589688e55e9cd149637a9
ShinSeoY <tjdudtls312@gmail.com>
2025-10-08 16:18:07 +0900

    - redirect uri 수정

--- src/main/kotlin/com/todaystock/api/common/security/OAuth2SuccessHandler.kt
+++ src/main/kotlin/com/todaystock/api/common/security/OAuth2SuccessHandler.kt
@@ -4,6 +4,7 @@ import com.todaystock.api.common.utils.JwtUtil
 import com.todaystock.api.entity.AuthProvider
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
@@ -11,12 +12,14 @@ import org.springframework.stereotype.Component
 
 @Component
 class OAuth2SuccessHandler(
-    private val jwtUtil: JwtUtil,
+        @Value("\${web.redirect-url}")
+        private val redirectUrl: String,
+        private val jwtUtil: JwtUtil,
 ) : AuthenticationSuccessHandler {
     override fun onAuthenticationSuccess(
-        request: HttpServletRequest,
-        response: HttpServletResponse,
-        authentication: Authentication,
+            request: HttpServletRequest,
+            response: HttpServletResponse,
+            authentication: Authentication,
     ) {
         val oauth2User = authentication.principal as org.springframework.security.oauth2.core.user.OAuth2User
 
@@ -29,6 +32,6 @@ class OAuth2SuccessHandler(
 
         val token = jwtUtil.generateToken(email, provider)
 
-        response.sendRedirect("http://localhost:5173/stock-alert?token=$token")
+        response.sendRedirect("$redirectUrl/stock-alert?token=$token")
     }
 }
