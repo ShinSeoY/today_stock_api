188feb27d817d4ab2ffebd927cc352cf67754af8
ShinSeoY <tjdudtls312@gmail.com>
2025-10-24 16:12:40 +0900

    - jwt key 고정

--- src/main/kotlin/com/todaystock/api/common/utils/JwtUtil.kt
+++ src/main/kotlin/com/todaystock/api/common/utils/JwtUtil.kt
@@ -2,35 +2,37 @@ package com.todaystock.api.common.utils
 
 import com.todaystock.api.entity.AuthProvider
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
-class JwtUtil {
-    private val key = Keys.secretKeyFor(SignatureAlgorithm.HS256)
-    private val expirationMs = 3600_000 // 1시간
+class JwtUtil(
+        @Value("\${jwt.secret}") private val secretB64: String
+) {
+    private val key = Keys.hmacShaKeyFor(io.jsonwebtoken.io.Decoders.BASE64.decode(secretB64))
+    private val expirationMs = 86_400_000 // 24시간
 
     fun generateToken(
-        email: String,
-        provider: AuthProvider,
+            email: String,
+            provider: AuthProvider,
     ): String {
         return Jwts.builder()
-            .setSubject(email)
-            .claim("provider", provider.name)
-            .setExpiration(Date(System.currentTimeMillis() + expirationMs))
-            .signWith(key)
-            .compact()
+                .setSubject(email)
+                .claim("provider", provider.name)
+                .setExpiration(Date(System.currentTimeMillis() + expirationMs))
+                .signWith(key)
+                .compact()
     }
 
     fun getEmailAndProviderFromToken(token: String): Pair<String, String> {
         val claims =
-            Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .body
+                Jwts.parserBuilder()
+                        .setSigningKey(key)
+                        .build()
+                        .parseClaimsJws(token)
+                        .body
 
         val email = claims.subject
         val provider = claims["provider"] as String
